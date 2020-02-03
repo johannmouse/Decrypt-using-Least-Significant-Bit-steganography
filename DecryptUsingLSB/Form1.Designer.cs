5243c71a896af701d68b9ada187f15dfd9623850
tqvinh <tqvinh@cmc.com.vn>
2020-02-03 18:05:33 +0700

    first commit

---
+++ DecryptUsingLSB/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace DecryptUsingLSB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(52, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(547, 281);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 382);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Decrypt);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(52, 468);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(547, 281);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 342);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(449, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(52, 339);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 824);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ DecryptUsingLSB/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DecryptUsingLSB
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        Bitmap bmpResize;
+        const string END_MARK = "vinhy9x";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog
+            {
+                Filter = "Image Files(*.jpg; .jpeg; .gif; .bmp; .png)|*.jpg; .jpeg; .gif; *.bmp ;*.png"
+            };
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                bmp = new Bitmap(new Bitmap(open.FileName));
+                if (bmp.Width <= bmp.Height && bmp.Height >= 281)
+                {
+                    bmpResize = new Bitmap(bmp, (int)(bmp.Width / ((float)bmp.Height / 281)), 281);
+                }
+                else if (bmp.Width > bmp.Height && bmp.Width >= 547)
+                {
+                    bmpResize = new Bitmap(bmp, 547, (int)(bmp.Height / ((float)bmp.Width / 547)));
+                    MessageBox.Show(bmpResize.Width + " - " + bmpResize.Height);
+                }
+                else
+                {
+                    bmpResize = bmp;
+                }
+                pictureBox1.Image = bmpResize;
+                textBox1.Text = open.FileName;
+            }
+        }
+
+        private void Decrypt(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (bmp == null)
+            {
+                MessageBox.Show("Choose the image containing message");
+                return;
+            }
+            StringBuilder rawMessage = new StringBuilder();
+            bool endMarkFound = false;
+            for (int i = 0; i < bmp.Height; i++)
+            {
+                count++;
+                if (AddBitOfMessage(i, ref rawMessage))
+                {
+                    endMarkFound = true;
+                    break;
+                }
+            }
+            Clipboard.SetText(rawMessage.ToString());
+            if (!endMarkFound)
+            {
+                MessageBox.Show("Can not find end point of the message in this picture.");
+            }
+        }
+        private bool AddBitOfMessage(int i, ref StringBuilder rawMessage)
+        {
+            for (int j = 0; j < bmp.Width; j++)
+            {
+                rawMessage.Append(Convert.ToString(bmp.GetPixel(i, j).R, 2).PadLeft(8, '0')[7]);
+                if (rawMessage.ToString().Contains(StringToBinary(END_MARK)))
+                {
+                    rawMessage.Remove(rawMessage.Length - 56, 56);
+                    richTextBox1.Text = BinaryToString(rawMessage.ToString());
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static string StringToBinary(string message)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in message)
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+        public static string BinaryToString(string binary)
+        {
+            try
+            {
+                List<Byte> byteList = new List<Byte>();
+
+                for (int i = 0; i < binary.Length; i += 8)
+                {
+                    byteList.Add(Convert.ToByte(binary.Substring(i, 8), 2));
+                }
+                return Encoding.ASCII.GetString(byteList.ToArray());
+            }
+            catch
+            {
+                MessageBox.Show("Invalid binary string.");
+                return string.Empty;
+            }
+        }
+    }
+}
